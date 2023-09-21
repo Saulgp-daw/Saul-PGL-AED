1f2b462024e655f09502d9040aed84fe131fdedd
Saulgp-daw <saulgp4a@gmail.com>
2023-09-21 19:16:00 +0100

    Hecha la tarea adicional

--- AED/TareaAdicional01.php
+++ AED/TareaAdicional01.php
@@ -1,15 +1,15 @@
 <?php
 /**
 *Generar un array de tamaño aleatorio entre 10 y 20 inclusives
-*los números generados serán del 1 al 30
+*los números generados serán del 1 al 20
 *Se debe mostrar el array generado y luego la siguiente salida
 *Aparece por repeticiones de más a menos 20 => 3 repeticiones etc
 */
 
 $arrayAleatorio = [];
 
-for($i = 0; $i < 9; $i++){
-    $num_aleatorio = random_int(1, 20);
+for($i = 0; $i < 20; $i++){
+    $num_aleatorio = random_int(10, 20);
     array_push($arrayAleatorio, $num_aleatorio);
 }
 rsort($arrayAleatorio);
