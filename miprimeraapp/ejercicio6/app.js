68bcf2fdafa9a35347c2e1d88a62cde9e78d9456
Saulgp-daw <saulgp4a@gmail.com>
2023-09-22 16:21:12 +0100

    Hechos los ejercicios

--- miprimeraapp/ejercicio6/app.js
+++ miprimeraapp/ejercicio6/app.js
@@ -1,7 +1,6 @@
 const {escribir} = require("./utils/manejarFichero");
 const {crearTabla} = require("./modelo/tabla");
 
-
-escribir("tabla.txt", crearTabla(process.argv[2]))
+escribir("tabla.txt", crearTabla(7))
     .then(console.log("ok grabado"))
     .catch(err => console.log(err));
