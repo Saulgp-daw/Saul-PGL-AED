3aa90d26d420d7c65f055a469922e1b0b5df5d84
Saulgp-daw <saulgp4a@gmail.com>
2023-10-04 20:30:39 +0100

    Feat(PGL): Ejercicio 17

---
+++ PGL/React/practica06/src/Components/practica17.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function practica17() {
+  return (
+    <>
+    <div>practica17</div>
+    <textarea name="textarea" id="textarea"></textarea>
+    <button onClick={recogerValores}></button>
+    <div className='respuesta'></div>
+    </>
+  )
+}
+
+function recogerValores(){
+    
+}
+
+export default practica17
