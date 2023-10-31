5f920a7cb6617c8dbe63f0e2f38ee2db24527597
Saulgp-daw <saulgp4a@gmail.com>
2023-10-31 15:17:43 +0000

    feat(PGL): Practica 47: contexto con router para establecer pokémon favorito finalizado y funcionando

--- PGL/React/practicas/src/components/Practica47/pokemonFavorito.tsx
+++ PGL/React/practicas/src/components/Practica47/pokemonFavorito.tsx
@@ -5,21 +5,29 @@ type Props = {}
 
 const PokemonFavorito = (props: Props) => {
   const { favorito, setfavorito } = useAppContext();
+
+  
+  console.log(favorito);
+  
   return (
     <div>
       <h3>Pokémon favorito</h3>
-      <div className="PokemonCard">
+      {Object.keys(favorito).length != 0 ?
+        (<div className="PokemonCard">
 
-        <div className={favorito.type1}>
-          <h3>{favorito.name}</h3>
-          <h5>Tipo: {favorito.type1}</h5>
-          <img src={favorito.front} alt={favorito.name} /><img src={favorito.front_shiny} alt={favorito.name} /><br />
-          <img src={favorito.back} alt={favorito.name} /><img src={favorito.back_shiny} alt={favorito.name} />
-          <h5>{favorito.weight} kg</h5>
-          <h5>{favorito.height} m</h5>
+          <div className={favorito.type1}>
+            <h3>{favorito.name}</h3>
+            <h5>Tipo: {favorito.type1}</h5>
+            <img src={favorito.front} alt={favorito.name} /><img src={favorito.front_shiny} alt={favorito.name} /><br />
+            <img src={favorito.back} alt={favorito.name} /><img src={favorito.back_shiny} alt={favorito.name} />
+            <h5>{favorito.weight} kg</h5>
+            <h5>{favorito.height} m</h5>
 
+          </div>
         </div>
-      </div>
+        ) :
+        (<p>No hay favorito seleccionado</p>)
+      }
     </div>
   )
 }
