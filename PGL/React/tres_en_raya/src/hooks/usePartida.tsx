a76dc7cde84c70f52b9d0c800901ce0855b02ca4
Saulgp-daw <saulgp4a@gmail.com>
2023-11-22 20:22:24 +0000

    feat(AED): empezado springboot

---
+++ PGL/React/tres_en_raya/src/hooks/usePartida.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import useTablero from './useTablero'
+
+type Props = {}
+
+const usePartida = (props: Props) => {
+    // useTablero();
+    //condición de victoria
+    return (
+        <div>usePartida</div>
+    )
+}
+
+export default usePartida
---
+++ PGL/React/tres_en_raya/src/hooks/useTablero.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+type Props = {}
+
+const useTablero = () => {
+    const [valor, setValor] = useState(false);
+
+    const [tablero, setTablero] = useState<string[][]>([
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+    ]);
+
+    function agregarACelda(fila: number, columna: number) {
+        if (!tablero[fila][columna]) {
+            const nuevoTablero = [...tablero];
+            const nuevoValor: string = valor ? "X" : "O";
+            setValor(!valor);
+            nuevoTablero[fila][columna] = nuevoValor;
+            setTablero(nuevoTablero);
+
+        }
+    }
+
+    const renderizarTabla = () => (
+        <table border={1} style={{ width: '300px', height: '300px' }}>
+            {tablero.map((fila, indiceFila) => (
+                <tr key={indiceFila}>
+                    {fila.map((celda, indiceColumna) => (
+                        <td key={indiceColumna} onClick={() => agregarACelda(indiceFila, indiceColumna)}>{celda}</td>
+                    ))}
+                </tr>
+            ))}
+        </table>
+    );
+    return { valor, setValor, tablero, setTablero, agregarACelda, renderizarTabla }
+}
+
+export default useTablero
