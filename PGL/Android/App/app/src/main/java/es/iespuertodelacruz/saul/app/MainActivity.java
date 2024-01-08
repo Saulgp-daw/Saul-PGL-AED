3dca9e54580991af581370a6d3dab05a0d67a2a8
Saulgp-daw <saulgp4a@gmail.com>
2024-01-08 20:13:10 +0000

    feat(PGL): Empezando dossier android

--- PGL/Android/App/app/src/main/java/es/iespuertodelacruz/saul/app/MainActivity.java
+++ PGL/Android/App/app/src/main/java/es/iespuertodelacruz/saul/app/MainActivity.java
@@ -1,8 +1,11 @@
 package es.iespuertodelacruz.saul.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +16,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }*/
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "First onCreate() calls", Toast.LENGTH_SHORT).show();
+    }*/
+
+    int contador = 0;
+       @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+           View btnPrueba = findViewById(R.id.btnPulsar);
+           btnPrueba.setOnClickListener(new View.OnClickListener() {
+            Fragment f = null;
+            @Override
+            public void onClick(View v) {
+                contador++;
+                if(contador % 2 == 0){
+                    f = new BlankFragmentUno();
+                }else{
+                    f = new BlankFragmentDos();
+                }
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainerView, f)
+                        .commit();
+            }
+        });
     }
+}
 }
