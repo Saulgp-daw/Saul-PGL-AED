5117cb4ecc248febcb2c46b93624dcfa10c36677
Saulgp-daw <saulgp4a@gmail.com>
2024-01-09 20:03:26 +0000

    feat(AED): Creado el hash para verificar la cuenta en swagger

--- AED/Springboot/Peliculas/src/main/java/es/iespuertodelacruz/sgp/peliculas/controller/LoginController.java
+++ AED/Springboot/Peliculas/src/main/java/es/iespuertodelacruz/sgp/peliculas/controller/LoginController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.iespuertodelacruz.sgp.peliculas.dto.RegisterDTO;
@@ -28,14 +30,24 @@ public class LoginController {
 		return ResponseEntity.ok(service.register(request));
 	}
 
-@PostMapping("/login")
-public ResponseEntity<String> authenticate(
-@RequestBody UserDetailsLogin request
-) {
-String token = service.authenticate(request);
-if(token == null)
-return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User/pass erróneo");
-else
-	return ResponseEntity.ok(token);
+	@PostMapping("/login")
+	public ResponseEntity<String> authenticate(
+			@RequestBody UserDetailsLogin request
+	) {
+	String token = service.authenticate(request);
+	if(token == null)
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User/pass erróneo");
+	else
+		return ResponseEntity.ok(token);
+	}
+	
+	@GetMapping("/registerverify")
+	public ResponseEntity<?> registerVerify(
+			@RequestParam(name = "usermail") String usermail,
+	        @RequestParam(name = "hash") String hash){
+			
+		
+				return null;
+		
 	}
 }
